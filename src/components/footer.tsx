15cf3a1db56b1d260cb5cc184c9b751f91dbebed
Raunak Bhagat <rabhagat31@gmail.com>
2024-08-31 22:55:53 -0700

    Add gap in footer sections

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -46,7 +46,7 @@ function ArrowIcon() {
 export default function Footer() {
   return (
     <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300 gap-0 md:gap-2">
         {content.links.map(({ name, link }, index) => (
           <li key={index}>
             <a
